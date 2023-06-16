6f20224d0d2fd1ef3fcf1c1f1550851fc60f9392
unknown <wakdeshravni@gmail.com>
2023-06-16 23:52:24 +0530

    trying to resolve error

--- src/components/Altcoins.jsx
+++ src/components/Altcoins.jsx
@@ -47,7 +47,7 @@ const Cryptocurrencies = ({ simplified }) => {
             <Link key={currency.uuid} to={`/crypto/${currency.uuid}`}>
               <Card
                 title={`${currency.rank}. ${currency.name}`}
-                extra={<img className="crypto-image" src={currency.iconUrl} />}
+                extra={<img className="crypto-image" src={currency.iconUrl} alt="Currency Icon" />}
                 hoverable
               >
                 <p>Price: {millify(currency.price)}</p>
--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -2,25 +2,25 @@ import React, { useState } from 'react';
 import HTMLReactParser from 'html-react-parser';
 import { useParams } from 'react-router-dom';
 import millify from 'millify';
-import { Col, Row, Typography, Select } from 'antd';
+import { Col, Row, Typography } from 'antd';
 import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
 
-import { useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } from '../services/cryptoApi';
+import { useGetCryptoDetailsQuery } from '../services/cryptoApi';
 import Loader from './Loader';
 
 const { Title, Text } = Typography;
-const { Option } = Select;
+// const { Option } = Select;
 
 
 const CryptoDetails = () => {
-  const {coinId} = useParams();
-  const [timeperiod, setTimeperiod] = useState('7d');
+  const { coinId } = useParams();
+  // const [timeperiod, setTimeperiod] = useState('7d');
   const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
   const cryptoDetails = data?.data?.coin;
 
   if (isFetching) return <Loader />;
 
-  const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
+  // const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
   const stats = [
     { title: 'Price to USD', value: `$ ${cryptoDetails?.price && millify(cryptoDetails?.price)}`, icon: <DollarCircleOutlined /> },
@@ -45,7 +45,7 @@ const CryptoDetails = () => {
           {data?.data?.coin.name} ({data?.data?.coin.symbol}) Price
         </Title>
         <p>
-        {cryptoDetails.name} live price in US Dollar (USD). View value statistics, market cap and supply.
+          {cryptoDetails.name} live price in US Dollar (USD). View value statistics, market cap and supply.
         </p>
       </Col>
 
